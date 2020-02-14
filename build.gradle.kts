ecd57e1e20e842eb6c235d34c3d8892ebf7f2007
Geoffrey Challen <challen@illinois.edu>
2020-02-14 14:33:14 -0600

    Added a test.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,11 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.3.0")
+}
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
 
 tasks {
