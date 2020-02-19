5d95299d8b6179a0f8d64cd9c6b8dbed3de943db
Geoffrey Challen <challen@illinois.edu>
2020-02-19 16:13:29 -0600

    Push full add example.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
 import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
@@ -14,8 +16,14 @@ fun main() {
             get("/") {
                 call.respondText(hello())
             }
-            get("/test") {
-                call.respondText("Testing")
+            get("/add/{first}/{second}") {
+                try {
+                    val first = call.parameters["first"]!!.toInt()
+                    val second = call.parameters["second"]!!.toInt()
+                    call.respondText((first + second).toString())
+                } catch (e: Exception) {
+                    call.respond(HttpStatusCode.BadRequest)
+                }
             }
         }
     }.start(wait = true)
