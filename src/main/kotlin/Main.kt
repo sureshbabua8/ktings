c66fa21d7ca9e4ae8766f0c17b35a760d1bd9073
Geoffrey Challen <challen@illinois.edu>
2020-02-26 13:08:17 -0600

    End of last lesson. Ready to accept JSON.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,10 @@
+package hello
+
+import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.gson.gson
 import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
 import io.ktor.response.respondText
@@ -10,21 +16,34 @@ import io.ktor.server.netty.Netty
 fun hello(): String {
     return "Hello, world!"
 }
-fun main() {
-    embeddedServer(Netty, 8008) {
-        routing {
-            get("/") {
-                call.respondText(hello())
-            }
-            get("/add/{first}/{second}") {
-                try {
-                    val first = call.parameters["first"]!!.toInt()
-                    val second = call.parameters["second"]!!.toInt()
-                    call.respondText((first + second).toString())
-                } catch (e: Exception) {
-                    call.respond(HttpStatusCode.BadRequest)
-                }
+data class AddResult(val first: Int, val second: Int, val result: Int)
+
+fun Application.adder() {
+    val counts: MutableMap<String, Int> = mutableMapOf()
+    install(ContentNegotiation) {
+        gson { }
+    }
+    routing {
+        get("/") {
+            call.respondText(hello())
+        }
+        get("/count/{first}") {
+            val firstCount = counts.getOrDefault(call.parameters["first"], 0) + 1
+            counts[call.parameters["first"].toString()] = firstCount
+            call.respondText(firstCount.toString())
+        }
+        get("/add/{first}/{second}") {
+            try {
+                val first = call.parameters["first"]!!.toInt()
+                val second = call.parameters["second"]!!.toInt()
+                val addResult = AddResult(first, second, first + second)
+                call.respond(addResult)
+            } catch (e: Exception) {
+                call.respond(HttpStatusCode.BadRequest)
             }
         }
-    }.start(wait = true)
+    }
+}
+fun main() {
+    embeddedServer(Netty, port = 8008, module = Application::adder).start(wait = true)
 }
--- src/test/kotlin/TestMain.kt
+++ src/test/kotlin/TestMain.kt
@@ -1,8 +1,20 @@
+package hello
+
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
+import io.ktor.application.Application
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.withTestApplication
 
 class TestMain : StringSpec({
-    "should greet properly" {
-        hello() shouldBe "Hello, world!"
+    "should retrieve root path properly" {
+        withTestApplication(Application::adder) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                response.status() shouldBe HttpStatusCode.OK
+                response.content shouldBe "Hello, world!"
+            }
+        }
     }
 })
